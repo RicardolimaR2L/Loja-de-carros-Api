c16d12d75b78db7af55307a3ddf0ef908165ab68
Ricardo Lima <92691384+RicardolimaR2L@users.noreply.github.com>
2024-01-31 01:36:40 -0300

    sdsds

--- midlewares/validarTokenJwt.ts
+++ midlewares/validarTokenJwt.ts
@@ -4,54 +4,54 @@ import jwt, { JwtPayload } from 'jsonwebtoken'
 
 export const ValidarTokenJWT =
   (handler: NextApiHandler) =>
-  (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
-    try {
-      const { MINHA_CHAVE_JWT } = process.env
+    (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
+      try {
+        const { MINHA_CHAVE_JWT } = process.env
 
-      if (!MINHA_CHAVE_JWT) {
-        return res.status(500).json({
-          erro: 'Env de chave JWT não informada na execução do projeto'
-        })
-      }
-      if (!req || !req.headers) {
+        if (!MINHA_CHAVE_JWT) {
+          return res.status(500).json({
+            erro: 'Env de chave JWT não informada na execução do projeto'
+          })
+        }
+        if (!req || !req.headers) {
+          return res
+            .status(401)
+            .json({ erro: 'Não foi possivel validar o token de acesso' })
+        }
+
+        if (req.method !== 'OPTIONS') {
+          const authorization = req.headers['authorization']
+          if (!authorization) {
+            return res
+              .status(401)
+              .json({ erro: 'Não foi possivel validar o token de acesso' })
+          }
+
+          const token = authorization.substring(7)
+
+          if (!token) {
+            return res
+              .status(401)
+              .json({ erro: 'Não foi possivel validar o token de acesso' })
+          }
+          const decoded = jwt.verify(token, MINHA_CHAVE_JWT) as JwtPayload
+
+          // if (!decoded || (decoded?.nivel !== 'admin' && req.method !== 'GET' && req.method !== 'PUT' && req.method !== 'DELETE' && req.method !== 'POST')) {
+          //   return res.status(401).json({
+          //     erro: 'Esse recurso só pode ser acessado por administradores'
+          //   })
+          // }
+          if (!req.query) {
+            req.query = {}
+          }
+          req.query.userId = decoded._id
+        }
+      } catch (error) {
+        console.log(error)
         return res
           .status(401)
           .json({ erro: 'Não foi possivel validar o token de acesso' })
       }
 
-      if (req.method !== 'OPTIONS') {
-        const authorization = req.headers['authorization']
-        if (!authorization) {
-          return res
-            .status(401)
-            .json({ erro: 'Não foi possivel validar o token de acesso' })
-        }
-
-        const token = authorization.substring(7)
-
-        if (!token) {
-          return res
-            .status(401)
-            .json({ erro: 'Não foi possivel validar o token de acesso' })
-        }
-        const decoded = jwt.verify(token, MINHA_CHAVE_JWT) as JwtPayload
-
-        if (!decoded || (decoded?.nivel !== 'admin' && req.method !== 'GET')) {
-          return res.status(401).json({
-            erro: 'Esse recurso só pode ser acessado por administradores'
-          })
-        }
-        if (!req.query) {
-          req.query = {}
-        }
-        req.query.userId = decoded._id
-      }
-    } catch (error) {
-      console.log(error)
-      return res
-        .status(401)
-        .json({ erro: 'Não foi possivel validar o token de acesso' })
+      return handler(req, res)
     }
-
-    return handler(req, res)
-  }
