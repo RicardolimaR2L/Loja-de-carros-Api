a137e7e7daadd974abbd00a786dac4d3a73d73f0
Ricardo Lima <92691384+RicardolimaR2L@users.noreply.github.com>
2024-01-31 01:36:35 -0300

    Nome dos endpoints

---
+++ src/pages/api/CarrosPublicos.ts
@@ -0,0 +1,39 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { CarrosModel } from '../../../models/CarroModel'
+import { conectarMongoDB } from '../../../midlewares/conectarMongoDb'
+import nc from 'next-connect'
+import { politicaCORS } from '../../../midlewares/politicaCors'
+
+const handler = nc().get(async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const id = req?.query?.id
+    if (!id) {
+      try {
+        const todosOsCarros = await CarrosModel.find()
+        return res.status(200).json(todosOsCarros)
+      } catch (error) {
+        console.error(error)
+        return res.status(500).json({
+          error:
+            'Não foi possível localizar carros cadastrados, verifique os dados de busca.'
+        })
+      }
+    }
+    const CarroEncontrado = await CarrosModel.findById(id)
+    if (!CarroEncontrado) {
+      return res.status(404).json('Carro não encontrado')
+    }
+    res.status(200).json(CarroEncontrado)
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({ error: 'Não foi possível localizar o carro' })
+  }
+})
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
+
+export default politicaCORS(conectarMongoDB(handler))
---
+++ src/pages/api/Login.ts
@@ -0,0 +1,40 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { conectarMongoDB } from '../../../midlewares/conectarMongoDb'
+import { UsuarioModel } from '../../../models/UsuarioModel'
+import Jwt from 'jsonwebtoken'
+import md5 from 'md5'
+import { RespostaPadraoMsg } from '../../../types/RespostaPadraoMsg'
+import { LoginResposta } from '../../../types/LoginResposta'
+import { politicaCORS } from '../../../midlewares/politicaCors'
+
+const endpointLogin = async (
+  req: NextApiRequest,
+  res: NextApiResponse<RespostaPadraoMsg | LoginResposta>
+) => {
+  const { MINHA_CHAVE_JWT } = process.env
+  if (!MINHA_CHAVE_JWT) {
+    return res.status(500).json({ erro: 'Env jwt não informada' })
+  }
+
+  if (req.method === 'POST') {
+    const { login, senha } = req?.body
+    const usuarioEncontrado = await UsuarioModel.find({
+      email: login,
+      senha: md5(senha)
+    })
+    if (usuarioEncontrado && usuarioEncontrado.length > 0) {
+      const usuario = usuarioEncontrado[0]
+
+      const token = Jwt.sign(
+        { _id: usuario._id, nivel: usuario?.nivel },
+        MINHA_CHAVE_JWT
+      )
+      return res
+        .status(200)
+        .json({ nome: usuario.nome, email: usuario.email, token })
+    }
+    return res.status(405).json({ erro: 'Usuário ou senha não encontrado' })
+  }
+  return res.status(405).json({ erro: 'metodo informado não é válido' })
+}
+export default politicaCORS(conectarMongoDB(endpointLogin))
