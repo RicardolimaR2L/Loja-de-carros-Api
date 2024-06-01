41212cdd647f6a3313d57a3c320e3043b922978e
Ricardo Lima <92691384+RicardolimaR2L@users.noreply.github.com>
2024-06-01 17:34:41 -0300

    Validação de carros

--- src/validators/carValidator.ts
+++ src/validators/carValidator.ts
@@ -1,3 +1,9 @@
+
+import { uploadImagemCosmic } from "@/midlewares/uploadImagemCosmic";
+import { CarMessagesHelper } from "@/pages/api/cars/helpers/messageHelper";
+import { NextApiRequest, NextApiResponse } from "next";
+
+
 const nameValidation = (name: string): boolean => {
   if (!name) {
     return false;
@@ -23,4 +29,36 @@ const priceValidation = (price: string): boolean => {
   return price.toString().length >= 5;
 };
 
-export {nameValidation, brandValidation,  modelValidation,priceValidation}
+const validateCar = (req: NextApiRequest, res: NextApiResponse) => {
+  const { name, model, brand, price } = req.body
+
+  if (!nameValidation(name)) {
+    return res.status(400).json({ erro: CarMessagesHelper.INVALID_NAME })
+  }
+
+  if (!modelValidation(model)) {
+    return res.status(400).json({ erro: CarMessagesHelper.INVALID_MODEL })
+  }
+  if (!brandValidation(brand)) {
+    return res.status(400).json({ erro: CarMessagesHelper.INVALID_BRAND })
+  }
+  if (!priceValidation(price)) {
+    return res.status(400).json({ erro: CarMessagesHelper.INVALID_PRICE })
+  }
+}
+
+const validatePhoto = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { file } = req.body
+  if (file) {
+    const photo = await uploadImagemCosmic(req);
+    if (!photo) {
+
+      return;
+      
+    }
+    return photo
+  }
+}
+
+
+export { nameValidation, brandValidation, modelValidation, priceValidation, validateCar, validatePhoto }
